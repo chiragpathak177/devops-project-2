3096b30ac98e02255d5fd5c01b95a9dc9fca9ec6
Chirag <chiragpathak72@gmail.com>
2026-09-19 13:57:02 +0530

    connect flask to postgres

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+import psycopg2
 
 app = Flask(__name__)
 
@@ -6,6 +8,21 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "DevOps Project 2 - Application is running!"
+
+
+@app.route("/db")
+def database():
+    try:
+        connection = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD")
+        )
+        connection.close()
+        return "Database connection successful!"
+    except Exception as e:
+        return f"Database connection failed: {e}", 500
 
 
 if __name__ == "__main__":
